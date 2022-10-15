dad9cfb0b4cb1d59c3fbbf9ba8166f996bd8b4a2
ted423 <priston910423@gmail.com>
2022-10-15 19:40:51 +0800

    Update flac.bat
    实现空参数不报错

--- Scrips/flac.bat
+++ Scrips/flac.bat
@@ -3,21 +3,25 @@ if "%~x1" ==".mkv" (
   ffmpeg -i "%~f1" -map 0:1 -c:a flac -compression_level 8 "%~p1%~n1.flac"
 ) else if "%~x1" ==".m2ts" (
   ffmpeg -i "%~f1" -map 0:1 -c:a flac -compression_level 8 "%~p1%~n1.flac"
-)else (
+) else (
   ffmpeg -i "%~f1" -c:a flac -compression_level 8 "%~p1%~n1.flac"
 )
-if "%~x2" ==".mkv" (
+if "%2"=="" (
+  exit
+) else if "%~x2" ==".mkv" (
   ffmpeg -i "%~f2" -map 0:1 -c:a flac -compression_level 8 "%~p2%~n2.flac"
 ) else if "%~x2" ==".m2ts" (
   ffmpeg -i "%~f2" -map 0:1 -c:a flac -compression_level 8 "%~p2%~n2.flac"
 ) else (
   ffmpeg -i "%~f2" -c:a flac -compression_level 8 "%~p2%~n2.flac"
 )
-if "%~x3" ==".mkv" (
+if "%3"=="" ( 
+  exit
+) else if "%~x3" ==".mkv" (
   ffmpeg -i "%~f3" -map 0:1 -c:a flac -compression_level 8 "%~p3%~n3.flac"
 ) else if "%~x3" ==".m2ts" (
   ffmpeg -i "%~f3" -map 0:1 -c:a flac -compression_level 8 "%~p3%~n3.flac"
-)else (
+) else (
   ffmpeg -i "%~f3" -c:a flac -compression_level 8 "%~p3%~n3.flac"
 )
 pause
