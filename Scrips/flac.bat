86d2986579c585e1b35336dc2ebe559c3af1341c
ted423 <priston910423@gmail.com>
2022-11-11 14:18:13 +0800

    Update flac.bat
    compression_level 12

--- Scrips/flac.bat
+++ Scrips/flac.bat
@@ -1,28 +1,28 @@
 @echo off
 if "%~x1" ==".mkv" (
-  ffmpeg -i "%~f1" -map 0:1 -c:a flac -compression_level 8 "%~p1%~n1.flac"
+  ffmpeg -i "%~f1" -map 0:1 -c:a flac -compression_level 12 "%~p1%~n1.flac"
 ) else if "%~x1" ==".m2ts" (
-  ffmpeg -i "%~f1" -map 0:1 -c:a flac -compression_level 8 "%~p1%~n1.flac"
+  ffmpeg -i "%~f1" -map 0:1 -c:a flac -compression_level 12 "%~p1%~n1.flac"
 ) else (
-  ffmpeg -i "%~f1" -c:a flac -compression_level 8 "%~p1%~n1.flac"
+  ffmpeg -i "%~f1" -c:a flac -compression_level 12 "%~p1%~n1.flac"
 )
 if "%2"=="" (
   goto end
 ) else if "%~x2" ==".mkv" (
-  ffmpeg -i "%~f2" -map 0:1 -c:a flac -compression_level 8 "%~p2%~n2.flac"
+  ffmpeg -i "%~f2" -map 0:1 -c:a flac -compression_level 12 "%~p2%~n2.flac"
 ) else if "%~x2" ==".m2ts" (
-  ffmpeg -i "%~f2" -map 0:1 -c:a flac -compression_level 8 "%~p2%~n2.flac"
+  ffmpeg -i "%~f2" -map 0:1 -c:a flac -compression_level 12 "%~p2%~n2.flac"
 ) else (
-  ffmpeg -i "%~f2" -c:a flac -compression_level 8 "%~p2%~n2.flac"
+  ffmpeg -i "%~f2" -c:a flac -compression_level 12 "%~p2%~n2.flac"
 )
 if "%3"=="" ( 
   goto end
 ) else if "%~x3" ==".mkv" (
-  ffmpeg -i "%~f3" -map 0:1 -c:a flac -compression_level 8 "%~p3%~n3.flac"
+  ffmpeg -i "%~f3" -map 0:1 -c:a flac -compression_level 12 "%~p3%~n3.flac"
 ) else if "%~x3" ==".m2ts" (
-  ffmpeg -i "%~f3" -map 0:1 -c:a flac -compression_level 8 "%~p3%~n3.flac"
+  ffmpeg -i "%~f3" -map 0:1 -c:a flac -compression_level 12 "%~p3%~n3.flac"
 ) else (
-  ffmpeg -i "%~f3" -c:a flac -compression_level 8 "%~p3%~n3.flac"
+  ffmpeg -i "%~f3" -c:a flac -compression_level 12 "%~p3%~n3.flac"
 )
 :end
 pause
